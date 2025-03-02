ecbdb20d20c9b7843067bd72d6288db01f60a4a1
Sandeep <sandeeptamta7453@gmail.com>
2025-03-02 11:55:47 +0530

    update section

--- src/Components/Section1.jsx
+++ src/Components/Section1.jsx
@@ -13,22 +13,22 @@ const Section1 = () => {
 
         {/* Quotes Section */}
         <div className='flex flex-col w-[100%] h-fit mt-20 md:mt-40 md:flex md:justify-center md:items-center'>
-          <h1 className='md:hidden mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
+          <h1 className='md:hidden  rounded-3xl mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
             🌿🌿"Discover the Magic of Greenery - Let Nature Soothe Your Soul.🌿"
           </h1>
           <h1 className='md:mt-[40px] rounded-3xl mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
             🌿"Breathe in the Freshness of Nature - A Symphony of Peace Awaits.🌿"
           </h1>
-          <h1 className='md:hidden mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
+          <h1 className='md:hidden  rounded-3xl  mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
             🌿🌿"Step into the Heart of Nature - Every Breeze Whispers a Tale.🌿"
           </h1>
-          <h1 className='md:hidden mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
+          <h1 className='md:hidden  rounded-3xl  mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
             🌿"Find Tranquility in Nature’s Embrace - Let Your Spirit Roam Free.🌿"
           </h1>
-          <h1 className='md:hidden mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
+          <h1 className='md:hidden  rounded-3xl  mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
             🌿🌿"Unwind in the Arms of Nature - Where Silence Speaks Volumes.🌿🌿"
           </h1>
-          <h1 className='md:hidden mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
+          <h1 className='md:hidden  rounded-3xl  mb-2 md:text-[2rem] mx-3 p-5 md:w-[45%] text-center font-semibold italic bg-gradient-to-r from-amber-500 to-pink-500 rounded-4xl'>
             🌿🌿"Revel in Nature’s Wonders - Every Moment is a Timeless Gift.🌿🌿"
           </h1>
         </div>
