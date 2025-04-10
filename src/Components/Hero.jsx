dfd80a42992a0b0f382b74a88e97c93d9c5fadca
Sandeep <sandeeptamta7453@gmail.com>
2025-04-10 16:00:40 +0530

    update blog

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { motion } from "motion/react";
 import bgImg from "../Images/Hero_bg.webp";
 
 const Hero = () => {
@@ -9,9 +9,13 @@ const Hero = () => {
       className="min-h-screen bg-cover bg-center flex justify-center items-center flex-col h-[100vh]"
       style={{ backgroundImage: `url(${bgImg})` }}
     >
-      <h1 className="text-7xl font-bold bg-gradient-to-r from-red-500 to-orange-500 text-transparent bg-clip-text md:mt-55 animate-bounce pl-2">
+      <motion.h1
+      initial={{scale:0.5, opacity:0}}
+      animate={{scale:1, opacity:1}}
+    transition={{duration:1, delay:0.1}}
+      className="text-7xl font-bold bg-gradient-to-r from-red-500 to-orange-500 text-transparent bg-clip-text md:mt-55 pl-2">
         Discover The Beauty Of Nature
-      </h1>
+      </motion.h1>
       <p className="text-center italic text-2xl bg-gradient-to-r from-slate-300 to-slate-100 text-transparent bg-clip-text max-w-6xl hidden md:block px-4">
       "Nature is not just a place to visit; it is where we belong. It is where the sunrises remind us of new beginnings, the forests teach us patience, and the mountains stand as proof that strength is found in stillness."
       </p>
