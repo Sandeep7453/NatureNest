32da126a0efbf8ba2858f0ac2a654f0faab130e3
Sandeep <sandeeptamta7453@gmail.com>
2025-06-26 14:21:56 +0530

    error fixing

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Add this line for GitHub Pages
   server: {
     port: 5173,
     host: true
   }
-})  
+})
