fbdbe9503b33c6ad4c9a28ea36b7bd002983d462
Sandeep <sandeeptamta7453@gmail.com>
2025-07-31 23:33:29 +0530

    update something

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ This is needed for Netlify/static deployment
-  server: {
-    port: 5173,
-    host: true
-  }
+  base: '/',
 })
